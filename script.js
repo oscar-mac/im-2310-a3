9917714111d18bc1fa30d28afc0ed0b3ae8f559b
oscar-mac <126298738+oscar-mac@users.noreply.github.com>
2023-06-02 16:37:15 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
+var isCalculatorOn = false;
+
+function turnOn() {
+  isCalculatorOn = true;
+  var buttons = document.getElementsByClassName('btn');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = false;
+  }
+}
+
+function turnOff() {
+  isCalculatorOn = false;
+  var buttons = document.getElementsByClassName('btn');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+  clearDisplay();
+}
+
 function appendToDisplay(value) {
+  if (isCalculatorOn) {
     var display = document.getElementById('display');
     display.value += value;
   }
-  
-  function clearDisplay() {
-    var display = document.getElementById('display');
-    display.value = '';
-  }
-  
-  function calculate() {
+}
+
+function clearDisplay() {
+  var display = document.getElementById('display');
+  display.value = '';
+}
+
+function calculate() {
+  if (isCalculatorOn) {
     var display = document.getElementById('display');
     var result = eval(display.value);
     display.value = result;
   }
-  
+}
