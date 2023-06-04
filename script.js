6bd962ddbf0953881ce477db32d6326168bf0be9
oscar-mac <126298738+oscar-mac@users.noreply.github.com>
2023-06-05 08:25:39 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Power On/Off
+
 var isCalculatorOn = false;
 
 function turnOn() {
@@ -6,16 +8,46 @@ function turnOn() {
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].disabled = false;
   }
+  displayHello();
+}
+
+function displayHello() {
+  var display = document.getElementById('display');
+  display.value = 'HELLO'; // Calculator says "HELLO" to foster a relationship the user has with the product and creates a playful user interaction.
+  disableButtons(); // Disable buttons during the "HELLO" display
+  setTimeout(function () {
+    clearDisplay();
+    enableButtons(); // Enable buttons after the "HELLO" display is cleared
+  }, 2000);
+}
+
+function disableButtons() {
+  var buttons = document.getElementsByClassName('btn');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+}
+
+function enableButtons() {
+  var buttons = document.getElementsByClassName('btn');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = false;
+  }
+}
+
+function clearDisplay() {
+  var display = document.getElementById('display');
+  display.value = '';
 }
 
 function turnOff() {
   isCalculatorOn = false;
   var buttons = document.getElementsByClassName('btn');
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].disabled = true;
-  }
+  disableButtons();
   clearDisplay();
 }
+
+// Calculator
 
 function appendToDisplay(value) {
   if (isCalculatorOn) {
@@ -26,16 +58,52 @@ function appendToDisplay(value) {
 
 function clearDisplay() {
   var display = document.getElementById('display');
-  display.value = '';
+  display.value = ''; // Clears display to ''
 }
 
 function calculate() {
   if (isCalculatorOn) {
     var display = document.getElementById('display');
-    var result = eval(display.value);
+    var result = new Function('return ' + display.value)(); // Creates return function and immediately calls it
     display.value = result;
   }
 }
 
-const audio = new Audio()
+// Key click (Feedback)
+
+const audio = new Audio() // Plays audio when function is called (button onclick)
 audio.src = "media/425187__mabdurrahman__calculatorclick.wav";
+
+// Info UI overlay
+
+function showOverlay() {
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "block"; // Sets display to block
+}
+
+function hideOverlay() {
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "none"; // Sets display to none (hide)
+}
+
+// debugging
+
+// function log() {
+//   console.log('Clicked');
+// };
+
+
+
+
+
+
+
+// KEYBOARD INPUT SUPPORT (TO DO)
+
+// INPUT ERROR FEEDBACK (TO DO)
+
+// GOODBYE ON POWER OFF (TO DO)
+
+// DISABLE BUTTON FEEDBACK ON POWER OFF
+
+// ADVANCED MATH USING .btn-disabled
